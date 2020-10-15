8b52a96b7bf6c932ab07d0f71a4b526d1d1f5d78
Dana Bliss <danajackson2@gmail.com>
2020-10-15 23:31:10 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,15 @@
 def key_for_min_value(name_hash)
- answer = nil
- num = 9999999
-  name_hash.each { |key, value|
-    if value < num
+  answer = nil
+  if name_hash.to_a == []
+    return nil
+  else
+    num = name_hash.to_a[0][1]
+  end
+  name_hash.each do |key, value|
+    if value <= num
       num = value
       answer = key
     end
-  }
+  end
   answer
 end
