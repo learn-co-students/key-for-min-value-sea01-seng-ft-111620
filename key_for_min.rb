aa9c1637e3caa4b8af1e36d0bd57986a9f2b1b0b
Ben Swanson <bswan0002@gmail.com>
2020-10-15 00:59:53 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,18 @@
 
 def key_for_min_value(name_hash)
 
+  lowVal = 999999
+  lowValKey = ""
+  
+  if name_hash.empty?
+    return nil
+  else
+    name_hash.each do |key, value|
+      if value < lowVal
+        lowVal = value
+        lowValKey = key
+      end
+    end
+  end
+  lowValKey
 end
